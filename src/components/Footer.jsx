164bf090cb0efa7a304d204cd4dfe19b08d36715
Kerry-Joseph <kerrykj8@gmail.com>
2022-12-23 16:35:25 -0700

    banner design | larger screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 export default function Footer(){
     return (
-        <h1>footer</h1>
+        <h1 className="hidden">footer</h1>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -65,38 +65,40 @@ export default function Main(){
   
 
     return (
-        <main className="relative">
-            <div className=" [&_div]:h-64 [&_div]:w-full">
-                {/* main - banner image */}
-                <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/banImgOne.webp"})`}}
-                className={`${bannerImage.imageOne} bg-[center_top_-7rem]`}></div>
-                <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/banImageTwo.jpg"})`}}
-                className={`${bannerImage.imageTwo} bg-center bg-top bg-cover`}></div>
-                <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/banImgThree.jpg"})`}}
-                className={`${bannerImage.imageThree} bg-center`}></div>
-                <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/banImageFour.png"})`}}
-                className={`${bannerImage.imageFour} bg-cover`}></div>
-                <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/banImageFive.png"})`}}
-                className={`${bannerImage.imageFive} bg-cover bg-center`}></div>
-            </div>
+        <main className="flex justify-center">
+            <main className="relative max-w-[1480px] w-full">
+                <div className="[&_div]:h-64 lg:[&_div]:h-[42rem] [&_div]:w-full">
+                    {/* main - banner image */}
+                    <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/banImgOne.webp"})`}}
+                    className={`${bannerImage.imageOne} bg-[center_top_-7rem] lg:bg-cover lg:bg-[center_top_-24rem]`}></div>
+                    <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/banImageTwo.jpg"})`}}
+                    className={`${bannerImage.imageTwo} bg-center bg-top bg-cover lg:bg-[center_top_-10rem]`}></div>
+                    <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/banImgThree.jpg"})`}}
+                    className={`${bannerImage.imageThree} bg-center lg:bg-cover`}></div>
+                    <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/banImageFour.png"})`}}
+                    className={`${bannerImage.imageFour} bg-cover`}></div>
+                    <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/banImageFive.png"})`}}
+                    className={`${bannerImage.imageFive} bg-cover bg-center`}></div>
+                </div>
 
-            <div
-            style={{background: "linear-gradient(0deg, rgba(209, 213, 219, 1) 80%, rgba(1,25,56,0) 100%)"}} 
-            className="h-48 w-full bg-gradient-to-t from-gray-300 -mt-10 z-20 [&_div]:h-[11.5rem] 
-            [&_div]:min-w-[8rem]  [&_div]:bg-white [&_div]:shadow-md [&_div]:rounded [&_div]:mx-1  
-            flex px-1 overflow-x-auto scrollbar">
-                {/* main - recommendations - mobile only */ }
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            {/* main - internet provider banner ad - mobile only */}
-            {/* main - sale banner ad - mobile only */}
-            {/* main - content block - 4 lighters per block  */}
+                <div
+                style={{background: "linear-gradient(0deg, rgba(209, 213, 219, 1) 80%, rgba(1,25,56,0) 100%)"}} 
+                className="lg:hidden h-48 w-full bg-gradient-to-t from-gray-300 -mt-10 z-20 [&_div]:h-[11.5rem] 
+                [&_div]:min-w-[8rem]  [&_div]:bg-white [&_div]:shadow-md [&_div]:rounded [&_div]:mx-1  
+                flex px-1 overflow-x-auto scrollbar">
+                    {/* main - recommendations - mobile only */ }
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                {/* main - internet provider banner ad - mobile only */}
+                {/* main - sale banner ad - mobile only */}
+                {/* main - content block - 4 lighters per block  */}
+            </main>
         </main>
     )
 }
