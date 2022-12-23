448f4490eb1e875e3db0c41451c8bca4af6ac5c9
Kerry-Joseph <kerrykj8@gmail.com>
2022-12-22 20:38:17 -0700

    nav main footer created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
 
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+
 function App() {
   return (
     <div className="App">
+      <Nav />
       {/* nav - search */}
       {/* nav - catagories */}
       {/* nav - deliver to/sign in - mobile only */}
       {/* nav - navigation button - bottom of screen - mobile only */}
+      <Main />
       {/* main - banner image */}
       {/* main - recommendations - mobile only */ }
       {/* main - internet provider banner ad - mobile only */}
       {/* main - sale banner ad - mobile only */}
       {/* main - content block - 4 lighters per block  */}
+      <Footer />
       {/* footer - back to top banner button */}
       {/* footer - company info */}
       {/* footer - logo/language/country */}
