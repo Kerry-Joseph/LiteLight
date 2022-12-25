ae66166cc5b4520b2209b8d3b98a535e185c3e5f
Kerry-Joseph <kerrykj8@gmail.com>
2022-12-24 17:50:35 -0700

    mobile bannder ads

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -66,9 +66,10 @@ export default function Main(){
 
     return (
         <main className="flex justify-center">
-            <main className="relative max-w-[1480px] w-full">
+            <main className="relative max-w-[1480px] w-full bg-gray-300">
+
                 <div className="[&_div]:h-64 lg:[&_div]:h-[42rem] [&_div]:w-full">
-                    {/* main - banner image */}
+                    {/* main - banner images */}
                     <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/banImgOne.webp"})`}}
                     className={`${bannerImage.imageOne} bg-[center_top_-7rem] lg:bg-cover lg:bg-[center_top_-24rem]`}></div>
                     <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/banImageTwo.jpg"})`}}
@@ -84,7 +85,7 @@ export default function Main(){
                 <div
                 style={{background: "linear-gradient(0deg, rgba(209, 213, 219, 1) 80%, rgba(1,25,56,0) 100%)"}} 
                 className="lg:hidden h-48 w-full bg-gradient-to-t from-gray-300 -mt-10 z-20 [&_div]:h-[11.5rem] 
-                [&_div]:min-w-[8rem]  [&_div]:bg-white [&_div]:shadow-md [&_div]:rounded [&_div]:mx-1  
+                [&_div]:min-w-[9rem]  [&_div]:bg-white [&_div]:shadow-md [&_div]:rounded [&_div]:mx-1  
                 flex px-1 overflow-x-auto scrollbar">
                     {/* main - recommendations - mobile only */ }
                     <div></div>
@@ -95,8 +96,17 @@ export default function Main(){
                     <div></div>
                     <div></div>
                 </div>
-                {/* main - internet provider banner ad - mobile only */}
-                {/* main - sale banner ad - mobile only */}
+
+                <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/Mobile_AD_FORMATS3-1.png"})`}}
+                className='h-20 w-full bg-center bg-cover mt-2 md:hidden'>
+                    {/* main - banner ad - mobile only */}
+                </div>
+
+                <div style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/Adver--Banners--150x750px-RIOT.jpg"})`}}
+                className='h-20 w-full bg-center bg-cover mt-2 md:hidden'>
+                    {/* main - banner ad - mobile only */}
+                </div>
+
                 <div
                 style={{background: "linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(255,255,255,1) 80%)"}}
                 className="hidden lg:block w-full h-96 -mt-96">
