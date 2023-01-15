db6e30a1e1ce9c84dbb978b9dd6cf4a21f0988cf
Kerry Joseph <kerrykj8@gmail.com>
2023-01-14 20:24:17 -0700

    wp:server - jwt passport created

--- server/validation.js
+++ server/validation.js
@@ -1,6 +1,9 @@
 const { compare } = require('bcryptjs')
 const { check, validationResult } = require('express-validator')
+const passport = require('passport')
+const { Strategy } = require('passport-jwt')
 const db = require('./db')
+const { SECRET } = require('./enviornment_variables')
 
 const validEmailCheck = check('email').isEmail().withMessage('Please provide avalid email.')
 
@@ -43,7 +46,45 @@ const loginValidation = check('email').custom( async(value, { req }) => {
 })
 
 
+const token = (req) => {
+  return req && req.cookies ? req.cookies['token'] : undefined
+}
+
+const jwtOptions = {
+  secretOrKey: SECRET,
+  jwtFromRequest: token
+}
+
+const jwtPassportStrategy = passport.use (
+  new Strategy(jwtOptions, async({ id }, done) => {
+    try {
+      const { rows } = await db.query('SELECT id, email, first_name, last_name, city, state FROM users WHERE id = $1',
+       [ id ])
+
+      if(!rows.length){
+        throw new Error('401 not authorized')
+      }
+
+      let user = {
+        id: rows[0].id,
+        email: rows[0].email,
+        first_name: rows[0].first_name,
+        last_name: rows[0].last_name,
+        city: rows[0].city,
+        state: rows[0].state,
+      }
+    } catch (err) {
+      console.log(err.message)
+      done(err.message, false)
+    }
+  })
+)
+
+const passportAuth = passport.authenticate('jwt', { session: false })
+
+
 module.exports = {
   registerValidationChain: [validEmailCheck, existingEmailCheck, passwordCheck, validationErrorCheck],
-  loginValidationChain: [loginValidation, validationErrorCheck]
+  loginValidationChain: [loginValidation, validationErrorCheck],
+  tokenPassportCheck: [jwtPassportStrategy, passportAuth]
 }
