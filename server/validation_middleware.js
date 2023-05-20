e0fd94851e73a0f212a17638918b8e5a6ae98eb7
Kerry-Joseph <kerrykj8@gmail.com>
2023-05-19 18:25:15 -0700

    vercel 5

--- server/validation_middleware.js
+++ server/validation_middleware.js
@@ -105,9 +105,33 @@ passport.use (
 const passportAuth = passport.authenticate('jwt', { session: false })
 
 
+// cors
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+
 module.exports = {
-  registerValidationChain: [validEmailCheck, existingEmailCheck, passwordCheck, validationErrorCheck],
-  loginValidationChain: [loginValidation, validationErrorCheck],
-  tokenPassportCheck: [passportAuth, validationErrorCheck],
-  updatePasswordValidationChain: [updatePasswordValidation, newPasswordCheck, validationErrorCheck]
+  registerValidationChain: [validEmailCheck, existingEmailCheck, passwordCheck, validationErrorCheck, allowCors(handler)],
+  loginValidationChain: [loginValidation, validationErrorCheck, allowCors(handler)],
+  tokenPassportCheck: [passportAuth, validationErrorCheck, allowCors(handler)],
+  updatePasswordValidationChain: [updatePasswordValidation, newPasswordCheck, validationErrorCheck, allowCors(handler)]
 }
